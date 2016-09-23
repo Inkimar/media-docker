028a9220c361299ea852b49f23b57aaacb085b91
Ingimar E <inkimar@gmail.com>
2016-09-23 10:14:06 +0200

    fetching sql-dump from IA instead-of 'buffalo'

--- get_enhanced-media_db-schema.sh
+++ get_enhanced-media_db-schema.sh
@@ -3,13 +3,13 @@ SCHEMA=mediaserver_for_nf-20160203.sql
 DST=mysql_media-autoload
 RELEASE=srv/releases
 
-echo "fetching the sql-dump, ${SCHEMA}, for the mediaserver , populated with data."
+echo "fetching  ${SCHEMA}, for the mediaserver , populated with data."
 SRC=https://archive.org/download/mediaserver_for_nf-20160203/${SCHEMA}
 echo "copying from ${SRC} to ${DST}"
 test -f $DST/$MEDIA_TAR || cd $DST && wget $SRC
 
 #Fetching artifact from Buffalo
-#SRC=/media/buffalo/dina-data/naturalist/integrated/${schema}
+#SRC=/media/buffalo/dina-data/naturalist/integrated/${SCHEMA}
 #echo "copying from ${SRC} to ${RELEASE} and ${DST}"
 #cp $SRC $DST
 
