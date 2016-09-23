facbe8c6f68f0bd948d57b423354d2ede8374904
Ingimar E <inkimar@gmail.com>
2016-09-23 10:52:53 +0200

    fetching from IA instead of 'buffalo'

--- get_enhanced-media_media-files.sh
+++ get_enhanced-media_media-files.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 MEDIA_TAR=media-files.tgz
-SRC=/media/buffalo/dina-data/naturalist/integrated/${MEDIA_TAR}
+SRC=https://archive.org/download/media-files/${MEDIA_TAR}
 DST=srv/data
 
 echo "copying mediafiles ${MEDIA_TAR} from ${SRC} to ${DST}"
-test -f $DST/$MEDIA_TAR || (cp $SRC $DST && cd $DST && tar xvfz ${MEDIA_TAR})
-#tar xvfz ${MEDIA_TAR}
-#echo "deletes the tar :  ${image_tar}"
-#rm ${image_tar} 
+
+test -f $DST/$MEDIA_TAR || (cd $DST && wget $SRC  && tar xvfz ${MEDIA_TAR})
+
+
+#SRC=/media/buffalo/dina-data/naturalist/integrated/${MEDIA_TAR}
+#test -f $DST/$MEDIA_TAR || (cp $SRC $DST && cd $DST && tar xvfz ${MEDIA_TAR})
+
