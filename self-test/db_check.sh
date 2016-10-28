0db8ce619206531042ab0d55e37b02353c03d22e
Ingimar Erlingsson <inkimar@gmail.com>
2016-10-28 16:31:48 +0200

    new name for the container

--- self-test/db_check.sh
+++ self-test/db_check.sh
@@ -1,1 +1,1 @@
-docker exec -it mediadocker_db.media_1 mysql -u mediaserver -pmediaserver -e "select * from media.ADMIN_CONFIG"
+docker exec -it mediadocker_dbmedia_1 mysql -u mediaserver -pmediaserver -e "select * from media.ADMIN_CONFIG"
--- self-test/db_count.sh
+++ self-test/db_count.sh
@@ -1,1 +1,1 @@
-docker exec -it mediadocker_db.media_1 mysql -u mediaserver -pmediaserver -e "select count(*) from media.MEDIA"
+docker exec -it mediadocker_dbmedia_1 mysql -u mediaserver -pmediaserver -e "select count(*) from media.MEDIA"
