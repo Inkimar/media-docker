97ee66729ec0b12ba7b84e81b8dc4c4b230d2d6b
Ingimar E <inkimar@gmail.com>
2016-11-01 16:24:29 +0100

    different deletes

--- docs/get_enhanced-media_war.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILE=mediaserver-nf.ear
-SRC=/media/buffalo/dina-data/naturalist/integrated/${FILE}
-DST=wildfly-custom
-RELEASE=srv/releases
-
-echo "copying from ${SRC} to ${RELEASE} and ${DST}"
-test -f $DST/$FILE || cp $SRC $RELEASE && cp $SRC $DST
