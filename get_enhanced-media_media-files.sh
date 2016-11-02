2183bfdb0950b419d18df03f0c6496c2c0d35e34
Ingimar E <inkimar@gmail.com>
2016-11-02 15:54:30 +0100

    extracting zipped file with --strip 1

--- get_enhanced-media_media-files.sh
+++ get_enhanced-media_media-files.sh
@@ -2,9 +2,7 @@
 MEDIA_FILES=media-files.tgz
 SRC=https://archive.org/download/media-files/${MEDIA_FILES}
 DST=srv/media
-echo "copying mediafiles ${MEDIA_FILES} from ${SRC} to ${DST}"
-test -f $MEDIA_FILES || (wget $SRC  && tar xvfz $MEDIA_FILES -C $DST)
+echo "copying mediafiles ${MEDIA_FILES} from ${SRC} to ${DST} -zip-up! "
+test -f $MEDIA_FILES || (wget $SRC  && tar xvfz $MEDIA_FILES -C $DST --strip 1)
 
-#SRC=/media/buffalo/dina-data/naturalist/integrated/${MEDIA_TAR}
-#test -f $DST/$MEDIA_TAR || (cp $SRC $DST && cd $DST && tar xvfz ${MEDIA_TAR})
-
+#tar xvfz $MEDIA_FILES -C $DST --strip 1
