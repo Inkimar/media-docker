8543db6f570dc5fab9bae104355047a04d505847
Ingimar E <inkimar@gmail.com>
2016-11-09 15:06:33 +0100

    will check for file then fetch file if it does not exist

--- get_enhanced-media_db-schema.sh
+++ get_enhanced-media_db-schema.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-SCHEMA=mediaserver_for_nf-20160203.sql
-SRC=https://archive.org/download/mediaserver_for_nf-20160203/${SCHEMA}
-
+FILE=mediaserver_for_nf-20160203.sql
+SRC=https://archive.org/download/mediaserver_for_nf-20160203/${FILE}
 DST=mysql_media-autoload
 
-echo "fetching  ${SCHEMA} from IA, for the mediaserver , populated with data."
-
-echo "copying from ${SRC} to ${DST}"
-test -f $DST/$SCHEMA || cd $DST && wget $SRC
+test -f $DST/$FILE || (cd $DST && wget $SRC)
 
 #Fetching artifact from Buffalo
 #SRC=/media/buffalo/dina-data/naturalist/integrated/${SCHEMA}
