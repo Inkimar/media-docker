99e4ed99899f20ca9bfe43cd5df85f464f9322b9
Ingimar E <inkimar@gmail.com>
2017-09-15 17:20:54 +0200

    work-in-progress

--- convenient-scripts/database_dump.sh
+++ convenient-scripts/database_dump.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 source ../.env
-
-
+NOW=`date +"%Y-%m-%d"`
 key="$1"
-echo $key
+#echo $key
+echo "Date is $NOW"
 
 case $key in 
      -d )
-     echo "dumps only the structure , your input is '$key'" 
+     echo "dumps only the structure (argument is '$key')" 
      docker exec -it mediadocker_db.media_1 sh -c  "mysqldump $key -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > /tmp/nf_media_barebone.sql"
      docker cp mediadocker_db.media_1:/tmp/nf_media_barebone.sql .
      ;;
-     --default)
-     echo "dumps structure & data, your input is '$key'" 
+     *)
+     echo "dumps structure & data, (no argument) " 
      docker exec -it mediadocker_db.media_1 sh -c  "mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > /tmp/nf_media_mysqldump.sql"
      docker cp mediadocker_db.media_1:/tmp/nf_media_mysqldump.sql .
      ;;
