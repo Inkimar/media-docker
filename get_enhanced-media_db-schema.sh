e43445016bbf9878ea45c988bf2bd9cc2aae8c49
Ingimar E <inkimar@gmail.com>
2017-11-19 16:31:12 +0100

    same syntax as other .sh fetching with 'wget'

--- get_enhanced-media_db-schema.sh
+++ get_enhanced-media_db-schema.sh
@@ -3,7 +3,7 @@ FILE=mediaserver_for_nf-20160203.sql
 SRC=https://archive.org/download/mediaserver_for_nf-20160203/${FILE}
 DST=mysql_media-autoload
 
-test -f $DST/$FILE || (cd $DST && wget $SRC)
+test -f $DST/$FILE || wget ${SRC} -P ${DST}
 
 #Fetching artifact from Buffalo
 #SRC=/media/buffalo/dina-data/naturalist/integrated/${SCHEMA}
