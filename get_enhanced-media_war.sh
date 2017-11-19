bc999ab83f3b33c8ea12b99e043f51f494b43069
Ingimar E <inkimar@gmail.com>
2017-11-19 16:31:58 +0100

    fetching from archive.org instead of mounted 'buffalo'

--- get_enhanced-media_war.sh
+++ get_enhanced-media_war.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
 # using the artifact from this project : https://github.com/IcingInk/mediaserver-nf-specific (private)
+
 FILE=mediaserver-nf.ear
-SRC=/media/buffalo/dina-data/naturalist/integrated/${FILE}
+
 SRC=https://archive.org/download/mediaserver-nf_201711/${FILE}
 DST=wildfly-custom/customization
 
-
-echo "fetches the artifact from Buffalo, relying on buffalo beeing mounted"
 echo "copying from ${SRC} to ${DST}"
 test -f $DST/$FILE || wget ${SRC} -P ${DST}
+
+#Fetching artifact from Buffalo
+#echo "fetches the artifact from Buffalo, relying on buffalo beeing mounted"
+#SRC=/media/buffalo/dina-data/naturalist/integrated/${FILE}
+#cp $SRC $DST
