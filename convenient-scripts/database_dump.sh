1b5c9d2f732199a90bf2740e1f09f5c365bbc234
Ingimar E <inkimar@gmail.com>
2018-02-16 15:45:49 +0100

    adding date to the mysqldump being made

--- convenient-scripts/database_dump.sh
+++ convenient-scripts/database_dump.sh
@@ -9,12 +9,12 @@ case $key in
      -d )
      echo "dumps only the structure (argument is '$key')" 
      docker exec -it mediadocker_db.media_1 sh -c  "mysqldump $key -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > /tmp/nf_media_barebone.sql"
-     docker cp mediadocker_db.media_1:/tmp/nf_media_barebone.sql .
+     docker cp mediadocker_db.media_1:/tmp/nf_media_barebone.sql ./nf_media_barebone_$NOW.sql
      ;;
      *)
      echo "dumps structure & data, (no argument) " 
      docker exec -it mediadocker_db.media_1 sh -c  "mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > /tmp/nf_media_mysqldump.sql"
-     docker cp mediadocker_db.media_1:/tmp/nf_media_mysqldump.sql .
+     docker cp mediadocker_db.media_1:/tmp/nf_media_mysqldump.sql ./nf_media_full_$NOW.sql
      ;;
 esac
 
